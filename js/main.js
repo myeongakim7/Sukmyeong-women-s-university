42bcaa6ea6cd49ae832b84f47207894a4fb5213c
myeongakim7 <113976428+myeongakim7@users.noreply.github.com>
2022-11-04 14:05:55 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// main.js
+
+console.log("main.js");
+
+$(function () {
+  // 배너 이미지 배열
+  let bgArr = [
+    "../images/contents/main-banner2.jpg",
+    "../images/contents/main-banner3.jpg",
+    "../images/contents/main-banner4.jpg",
+  ];
+
+  let bg_img_num = 0; // 이미지 번호
+
+  // 배경이미지 변경
+  // $("#main-banner .container").css({
+  //   "background-image": `url(${bgArr[bg_img_num]})`,
+  // }); 두 번 입력하면 안됨!
+
+  // 배경 재생
+  setInterval(function () {
+    bg_img_num++; // 이미지 번호 카운트 증가
+    if (bg_img_num >= 3) {
+      bg_img_num = 0;
+    }
+    $("#main-banner .container").css({
+      "background-image": `url(${bgArr[bg_img_num]})`,
+    });
+  }, 3000);
+});
+// 1초가 1000이구나
+// ``< 백틱 <  `편하게 쓸 때 텍스트 ${함수}`
