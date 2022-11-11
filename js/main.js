1ad2720b4334342a51e63f7153abf39fdc610ad5
myeongakim7 <113976428+myeongakim7@users.noreply.github.com>
2022-11-11 17:48:19 +0900

    모바일 about, location, sub3-1 추가
    -모바일 버전 about, location, sub3-1 추가

--- js/main.js
+++ js/main.js
@@ -34,11 +34,22 @@ $(function () {
 
 // 사이트맵 버튼
 $(function () {
+  // PC 사이트맵, 모바일 메뉴 버튼
   $("#btn-toggle").on("click", function () {
     $("#site-map-menu").toggleClass("show");
   });
 
   $("#close").on("click", function () {
     $("#site-map-menu").removeClass("show");
+  });
+});
+// ** 자바스크립트도 PC랑 모바일 구분해서 함수쓰기!!!!** 같이 쓰면 섞여서 구분도 안되고 적용이 잘 안됨....
+$(function () {
+  // GNB 1단계 a 기본 이벤트 제거
+  $("#site-map-menu #site-menu .depth1 > li > a").on("click", function (event) {
+    // 여기다 toggle 기능 추가
+    // 사이트맵 1번 a 태그의 형제 관계에 있는 depth2 ul 태그를 선택하여 클릭할 경우 show 클래스를 추가하여 display:block 으로 보이게 한다!  (당연히 기본적으로 모바일 style 시트에서 depth2 는 display:none 상태여야 한다.)
+    $(this).next().toggleClass("show");
+    event.preventDefault();
   });
 });
